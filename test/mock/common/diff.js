84d7dab0560d4d0b86a53198a8cd6a55e7277b50
startheart <704469176@qq.com>
2019-03-31 23:29:42 +0800

    add diff test

--- test/mock/common/diff.js
+++ test/mock/common/diff.js
@@ -130,3 +130,15 @@ export const case9_3 = new Config({
     '[3]': 'd'
   }
 })
+
+export const case9_4 = new Config({
+  in: {
+    new: {
+        "a": 1, "b": 2, "c": "str", "d": { "e": [2, { "a": 4 }, 5] }, "f": true, "h": [1], "g": { "a": [1, 2], "j": 111 }
+    },
+    old: {
+        "a": [], "b": "aa", "c": 3, "d": { "e": [3, { "a": 3 }] }, "f": false, "h": [1, 2], "g": { "a": [1, 1, 1], "i": "delete" }, "k": "del"
+    }
+  },
+  out: { "a": 1, "b": 2, "c": "str", "d.e[0]": 2, "d.e[1].a": 4, "d.e[2]": 5, "f": true, "h": [1], "g.a": [1, 2], "g.j": 111, "g.i": "", "k": "" }
+})
