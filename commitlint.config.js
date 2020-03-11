9d6a29d3525da3b8c9ff8a454cc614c1f08a0b7f
startheart <704469176@qq.com>
2020-03-11 18:55:10 +0800

    docs:add commit convention doc

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = {extends: ['@commitlint/config-angular']};
+module.exports = {
+    extends: ['@commitlint/config-angular'],
+    rules: {
+        'type-enum': [2, 'always', [
+          "feat", "fix", "polish", "docs", "style", "refactor", "perf", "test", "workflow", "ci", "chore", "types", "revert"
+        ]],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never'],
+     }
+};
